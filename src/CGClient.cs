13283f482e7b83409b96e8373f1e53d927f95bb0
roberto montalti <roberto.montalti00@gmail.com>
2021-07-31 14:36:43 +0200

    added market data for multiple coins, using varargs. added then CoinHistory, which gives some other, will probably be unused

--- src/CGClient.cs
+++ src/CGClient.cs
@@ -4,15 +4,15 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 
+using CoinManager.ApiData;
 using CoinManager.Shared;
 
 namespace CoinManager.API
 {
     public class CoingeckoClient
     {
-        private HttpClient http;
-
         public List<SimpleCoin> CoinsList { get; private set; }
+        private HttpClient http;
 
         public CoingeckoClient()
         {
@@ -21,12 +21,34 @@ namespace CoinManager.API
 
         public async Task<List<SimpleCoin>> GetCoinsList() 
         {
-            var data = (await http.GetAsync(CGPaths.Base + CGPaths.CoinsList)).Content;
+            var data = (await http.GetAsync(CGPaths.CoinsList)).Content;
             var dataString = await data.ReadAsStringAsync();
             return JsonSerializer
                 .Deserialize<List<SimpleCoin>>(
                     await data.ReadAsStringAsync()
                     );
+        }
+
+        public async Task<CoinInfo> GetCoinHistory(int coinId)
+        {
+            string completeUrl = String.Format(CGPaths.CoinHistoryId, coinId);
+            var data = (await http.GetAsync(completeUrl)).Content;
+            string dataString = await data.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<CoinInfo>(dataString);
+        }
+
+        public async Task<List<CoinMarket>> GetMarketData(string vsSymbol, params string[] ids)
+        {
+            string completeUrl = CGPaths.CoinsMarket + "?vs_currency=" + vsSymbol;
+            if(ids.Length != 0)
+            {
+                string paramChain = string.Join(",", ids);
+                completeUrl += $"&ids={paramChain}";
+            }
+            var data = (await http.GetAsync(completeUrl)).Content;
+            var dataString = await data.ReadAsStringAsync();
+            Console.WriteLine(dataString);
+            return JsonSerializer.Deserialize<List<CoinMarket>>(dataString);
         }
     }
 }
