b93a8a323909fa3f3f38ffff87d1be3058cda373
roberto montalti <roberto.montalti00@gmail.com>
2021-07-31 14:37:20 +0200

    added some more models to support coingecko reposnses

--- src/Shared.cs
+++ src/Shared.cs
@@ -69,6 +69,35 @@ namespace CoinManager.Shared
         public Image image { get; set; }
     }
 
+    public class CoinMarket
+    {
+        public string id { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
+        public string image { get; set; }
+        public double? current_price { get; set; }
+        public long? market_cap { get; set; }
+        public int? market_cap_rank { get; set; }
+        public double? fully_diluted_valuation { get; set; }
+        public double? total_volume { get; set; }
+        public double? high_24h { get; set; }
+        public double? low_24h { get; set; }
+        public double? price_change_24h { get; set; }
+        public double? price_change_percentage_24h { get; set; }
+        public double? market_cap_change_24h { get; set; }
+        public double? market_cap_change_percentage_24h { get; set; }
+        public double? circulating_supply { get; set; }
+        public double? total_supply { get; set; }
+        public double? max_supply { get; set; }
+        public double? ath { get; set; }
+        public double? ath_change_percentage { get; set; }
+        public DateTime? ath_date { get; set; }
+        public double? atl { get; set; }
+        public double? atl_change_percentage { get; set; }
+        public DateTime? atl_date { get; set; }
+        public object roi { get; set; }
+        public DateTime? last_updated { get; set; }
+    }
     
     public static class StringExtensions
     {
@@ -88,10 +117,11 @@ namespace CoinManager.ApiData
         public static string Base =                "https://api.coingecko.com/api/v3";
         public static string Ping =                Base + "/ping";
         public static string SimplePrice =         Base + "/simple/price";
-        public static string SimpleWithAddress =   Base + "/simple/token/price/{id}";
+        public static string SimpleWithAddress =   Base + "/simple/token/price/{0}";
         public static string CoinsList =           Base + "/coins/list";
-        public static string CoinsMarket =         Base + "/coins/market";
-        public static string CoinsDataId =         Base + "/coins/{id}";
+        public static string CoinsMarket =         Base + "/coins/markets";
+        public static string CoinsDataId =         Base + "/coins/{0}";
+        public static string CoinHistoryId =       Base + "/coins/{0}/history";
         public static string Trending =            Base + "/search/trending";
     }
 }
