d36d46a7710ef54ce8e8ee07f3ecdaf4007038cc
roberto montalti <roberto.montalti00@gmail.com>
2021-08-04 14:00:08 +0200

    testing market data

--- src/Main.cs
+++ src/Main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -11,20 +12,39 @@ public class Startup
     public static void Main(string[] args)
     {
         const string TITLE = "CoinManager Desktop";
-
         new Eto.Forms.Application().Run(new CoinsForm(TITLE));
         string vs = "usd";
+        var names = new List<string>();
         var client = new CoingeckoClient();
         List<SimpleCoin> list = null;
         Task.Run(async () => 
                 {
-                list = await client.GetCoinsList();                
-                var mktList = await client.GetMarketData(vs, "bitcoin", "ethereum");
-                mktList.ForEach((c) => 
+                list = await client.GetCoinsList();        
+                var onlyIds = list.Select(c => c.id);
+                Func<string[], Task> api = async (string[] list) => {
+                    var mktList = await client.GetMarketData(vs, list);
+                    mktList.ForEach((c) => 
                         {
-                            Console.WriteLine(c.name);
+                            names.Add(c.name);
                         });
+                };
+                var l = new List<string>();
+                for(int i = 0; i < onlyIds.Count(); i++)
+                {
+                    l.Add(onlyIds.ElementAt(i));
+                    if(i%10 == 0)
+                    {
+                        l.Clear();    
+                        await api(l.ToArray());
+                    }
+                }
                 }).Wait();
+
+        foreach(var n in names)
+        {
+            Console.WriteLine(n);
+        }
+
     }
 }
 
