8ee370785a6a37513adb9783a4d2f3dc35bb375d
roberto montalti <roberto.montalti00@gmail.com>
2021-08-06 19:08:52 +0200

    testing EF

--- src/Main.cs
+++ src/Main.cs
@@ -8,6 +8,7 @@ using CoinManager.GUI;
 using CoinManager.API;
 using CoinManager.DB;
 using CoinManager.Shared;
+using CoinManager.EF;
 
 public class Startup
 {
@@ -17,18 +18,24 @@ public class Startup
         new Eto.Forms.Application().Run(new CoinsForm(TITLE));
         var client = new CoingeckoClient();
         var connString = "Host=localhost;Username=rob;Password=rob;Database=coinmanager";
-        var db = new DbHelper(connString);
 
         List<CoinMarket> items;
         using (StreamReader r = new StreamReader("./market.json"))
         {
             string json = r.ReadToEnd();
             items = JsonSerializer.Deserialize<List<CoinMarket>>(json);
-        }
-        Task.Run(async () => 
+            var db = new CMDbContext("localhost", "coinmanager", "rob", "rob"); 
+            var bitcoin = db.crypto.Find("bitcoin");
+            var sku = db.userstandard.Find(0);
+            Console.WriteLine(bitcoin.id + " " + bitcoin.currentprice);
+            Console.WriteLine(sku);
+       }
+        /*
+        Task.Run(async () =>
                 {
                     await db.InsertCryptos(items);
                 }).Wait();
+                */
     }
 }
 
