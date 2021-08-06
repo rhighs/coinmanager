781c0041f91076ff6258b4ffe006ef4887d933d7
roberto montalti <roberto.montalti00@gmail.com>
2021-08-06 16:37:28 +0200

    populate db with crypto

--- src/Database.cs
+++ src/Database.cs
@@ -1,6 +1,10 @@
 using System;
 using Npgsql;
+using Npgsql.PostgresTypes;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using CoinManager.Shared;
 
 namespace CoinManager.DB
 {
@@ -24,6 +28,56 @@ namespace CoinManager.DB
                 {
                     Console.WriteLine(reader.GetString(0));
                 }
+            }
+        }
+
+        public async Task InsertCryptos(List<CoinMarket> list)
+        {
+            await using var conn = new NpgsqlConnection(connectionString);
+            await conn.OpenAsync();
+
+            foreach(var crypto in list) 
+            {
+                if(crypto.id.Length > 20) continue;
+                var cmd = new NpgsqlCommand("INSERT INTO crypto (id, name, symbol, currentprice, imageurl, marketcap, marketcaprank, circulatingsupply, totalvolume) VALUES (:id, :name, :symbol, :currentprice, :imageurl, :marketcap, :marketcaprank, :circulatingsupply, :totalvolume);", conn);
+ 
+                NpgsqlParameter id = new NpgsqlParameter("id", NpgsqlTypes.NpgsqlDbType.Text);
+                id.Value = crypto.id;
+
+                NpgsqlParameter name = new NpgsqlParameter("name", NpgsqlTypes.NpgsqlDbType.Text);
+                name.Value = crypto.name;
+
+                NpgsqlParameter sym = new NpgsqlParameter("symbol", NpgsqlTypes.NpgsqlDbType.Text);
+                sym.Value = crypto.symbol;
+
+                NpgsqlParameter curr = new NpgsqlParameter("currentprice", NpgsqlTypes.NpgsqlDbType.Real);
+                curr.Value = crypto.current_price;
+
+                NpgsqlParameter image = new NpgsqlParameter("imageurl", NpgsqlTypes.NpgsqlDbType.Text);
+                image.Value = crypto.image;
+
+                NpgsqlParameter mkt = new NpgsqlParameter("marketcap", NpgsqlTypes.NpgsqlDbType.Real);
+                mkt.Value = crypto.market_cap;
+
+                NpgsqlParameter mktrank = new NpgsqlParameter("marketcaprank", NpgsqlTypes.NpgsqlDbType.Real);
+                mktrank.Value = crypto.market_cap_rank;
+
+                NpgsqlParameter circ = new NpgsqlParameter("circulatingsupply", NpgsqlTypes.NpgsqlDbType.Real);
+                circ.Value = crypto.circulating_supply;
+
+                NpgsqlParameter tot = new NpgsqlParameter("totalvolume", NpgsqlTypes.NpgsqlDbType.Real);
+                tot.Value = crypto.total_volume;
+
+                cmd.Parameters.Add(id);
+                cmd.Parameters.Add(name);
+                cmd.Parameters.Add(sym);
+                cmd.Parameters.Add(curr);
+                cmd.Parameters.Add(image);
+                cmd.Parameters.Add(mkt);
+                cmd.Parameters.Add(mktrank);
+                cmd.Parameters.Add(circ);
+                cmd.Parameters.Add(tot);
+                await cmd.ExecuteNonQueryAsync();
             }
         }
     }
--- src/Main.cs
+++ src/Main.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 using CoinManager.GUI;
 using CoinManager.API;
+using CoinManager.DB;
 using CoinManager.Shared;
 
 public class Startup
@@ -17,48 +15,20 @@ public class Startup
     {
         const string TITLE = "CoinManager Desktop";
         new Eto.Forms.Application().Run(new CoinsForm(TITLE));
-        string vs = "usd";
-        var names = new List<string>();
         var client = new CoingeckoClient();
-        var list = new List<SimpleCoin>();
+        var connString = "Host=localhost;Username=rob;Password=rob;Database=coinmanager";
+        var db = new DbHelper(connString);
+
+        List<CoinMarket> items;
+        using (StreamReader r = new StreamReader("./market.json"))
+        {
+            string json = r.ReadToEnd();
+            items = JsonSerializer.Deserialize<List<CoinMarket>>(json);
+        }
         Task.Run(async () => 
                 {
-                list = await client.GetCoinsList();        
-                var onlyIds = list.Select(c => c.id);
-                var txtStream = File.AppendText("./market.txt");
-                Func<string[], Task> api = async (string[] _list) => {
-                    var mktList = await client.GetMarketData(vs, _list);
-                    mktList.ForEach((c) => 
-                        {
-                            if (c.market_cap_rank is not null && c.market_cap_rank < 300)
-                            {
-                                Console.WriteLine("success");
-                                Console.WriteLine(c.market_cap_rank + " " + c.market_cap_rank.GetType().ToString());
-                            }
-                            txtStream.WriteLine(
-                                new UTF8Encoding(true)
-                                    .GetBytes(c.name + " " + c.market_cap_rank.ToString() + "\n")
-                                );
-                            names.Add(c.name);
-                        });
-                };
-                var l = new List<string>();
-                for(int i = 0; i < onlyIds.Count(); i++)
-                {
-                    var id = onlyIds.ElementAt(i);
-                    l.Add(id);
-                    if(i%100== 0)
-                    {
-                        var arr = l.ToArray();
-                        await api(l.ToArray());
-                        l.Clear();    
-                    }
-                }
+                    await db.InsertCryptos(items);
                 }).Wait();
-        foreach(var n in names)
-        {
-            Console.WriteLine(n);
-        }
     }
 }
 
