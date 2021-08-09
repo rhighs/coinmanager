5d809fb56c0a9ac407ce966d38206fb0e9292be3
roberto montalti <roberto.montalti00@gmail.com>
2021-08-09 15:41:47 +0200

    fixed data types conversions

--- src/Main.cs
+++ src/Main.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 using CoinManager.GUI;
 using CoinManager.API;
+using CoinManager.Util;
 using CoinManager.EF;
 
 public class Startup
@@ -9,12 +11,13 @@ public class Startup
     public static void Main(string[] args)
     {
         const string TITLE = "CoinManager Desktop";
+        var db = new CMDbContext("localhost", "CoinManager", "rob", "rob");
+        var pop = new Populator();
+        Task.Run(async () =>
+            {
+                await pop.GenerateCryptos(200);
+            }).Wait();
         new Eto.Forms.Application().Run(new CoinsForm(TITLE));
-        var client = new CoingeckoClient();
-        var db = new CMDbContext("localhost", "coinmanager", "rob", "rob"); 
-        var bitcoin = db.Crypto.Find("bitcoin");
-        Console.WriteLine(bitcoin);
-        Console.WriteLine(bitcoin.Id + " " + bitcoin.CurrentPrice);
     }
 }
 
