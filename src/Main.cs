0320a8bbdcefee0c05caa0d80326062574a39a78
jurisimo00 <juri.simoncini@studio.unibo.it>
2021-08-10 16:20:30 +0200

    fix wallet layout

--- src/Main.cs
+++ src/Main.cs
@@ -11,10 +11,11 @@ public class Startup
     public static void Main(string[] args)
     {
         const string TITLE = "CoinManager Desktop";
-        var db = new CMDbContext("localhost", "CoinManager", "rob", "rob");
+        var db = new CMDbContext("localhost", "coinmanager", "rob", "rob");
         var pop = new Populator();
-        pop.GenerateCryptos(200).Wait();
+        //pop.GenerateCryptos(200).Wait();
         new Eto.Forms.Application().Run(new CoinsForm(TITLE));
+        
     }
 }
 
