a37a4568dc7d3b4137ab4dba1deef70e1f526c68
roberto montalti <roberto.montalti00@gmail.com>
2021-08-22 15:34:39 +0200

    added buy sell dialog

--- src/Dialogs.cs
+++ src/Dialogs.cs
@@ -33,7 +33,12 @@ namespace CoinManager.GUI
                 Size = new Size(400, 100),
                 Command = new Command((sender, e) =>
                 {
-                    Console.WriteLine("new popup should come out");
+                    var dialog = new Dialog
+                    {
+                        Size = new Size(Size.Width, Size.Height/2),
+                        Content = new BuySellDialog(crypto)
+                    };
+                    dialog.ShowModal();
                 })
             };
 
@@ -65,6 +70,104 @@ namespace CoinManager.GUI
                 Image = new Bitmap(memStream),
                 Size = new Size(100, 100)
             };
+        }
+    }
+
+    public class BuySellDialog : Panel
+    {
+        private Crypto _crypto;
+        private CMDbContext db;
+        private Button confirmButton;
+        private DropDown changeAction;
+
+        private readonly Size TABLE_SPACING = new Size(20, 20);
+        private readonly Padding TABLE_PADDING = new Padding(20);
+
+        public BuySellDialog(Crypto crypto)
+        {
+            _crypto = crypto;
+
+            var cryptoName1 = new Label { Text = "Dollari" };
+            var cryptoName2 = new Label { Text = _crypto.Name };
+            var cryptoQty1 = new TextBox { PlaceholderText = "USD" };
+            var cryptoQty2 = new TextBox { PlaceholderText = _crypto.Symbol.ToUpper() };
+
+            confirmButton = CreateButton();
+            changeAction = CreateDropDown();
+
+            var labels = new TableRow 
+            {
+                Cells =
+                {
+                    new TableCell(cryptoName1, true),
+                    new TableCell(cryptoName2, true),
+                }
+            };
+            var inputs = new TableRow 
+            {
+                Cells =
+                {
+                    new TableCell(cryptoQty1, true),
+                    new TableCell(cryptoQty2, true),
+                }
+            };
+            var actions = new TableRow 
+            {
+                Cells =
+                {
+                    new TableCell(changeAction, true),
+                    new TableCell(confirmButton, true)
+                }
+            };
+
+            Content = CreateTable(labels, inputs, actions);
+        }
+
+        private TableLayout CreateTable(params TableRow[] rows)
+        {
+            var table = new TableLayout();
+            table.Padding = TABLE_PADDING;
+            table.Spacing = TABLE_SPACING;
+            foreach(var row in rows) 
+                table.Rows.Add(row);
+            return table;
+        }
+
+        private Button CreateButton()
+        {
+            var button = new Button
+            {
+                Text = "Conferma"
+            };
+            return button;
+        }
+
+        private DropDown CreateDropDown()
+        {
+            var dropDown = new DropDown
+            {
+                Items = 
+                { 
+                    new ListItem { Text = "Compra" },
+                    new ListItem { Text = "Vendi" }
+                }
+            };
+            var buyCommand = new Command((sender, e) =>
+            {
+                Console.WriteLine("Compra");
+            });
+            var sellCommand = new Command((sender, e) =>
+            {
+                Console.WriteLine("Vendi");
+            });
+            dropDown.SelectedIndex = 0;
+            confirmButton.Command = buyCommand; //buy set as default
+            dropDown.SelectedIndexChanged += (sender, e) => 
+            {
+                confirmButton.Command = dropDown.SelectedIndex == 0 
+                    ? buyCommand : sellCommand;
+            };
+            return dropDown;
         }
     }
 }
