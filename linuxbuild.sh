9653839cca1d8140e6925c6d3f71b6e5498f40b5
roberto montalti <roberto.montalti00@gmail.com>
2021-09-04 22:27:51 +0200

    update scripts

--- linuxbuild.sh
+++ linuxbuild.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true;
+mv ./bin/Release/net5.0/linux-x64/publish/coinmanager ./exe/
--- winbuild.bat
+++ winbuild.bat
@@ -1,1 +1,2 @@
 dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true
+mv .\bin\Release\net5.0\win-x64\publish\coinmanager.exe .\exe\
