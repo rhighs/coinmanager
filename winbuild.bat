70016bb5d57d459060de86edc545cafca329cf15
roberto montalti <roberto.montalti00@gmail.com>
2021-09-04 22:13:23 +0200

    fixes for windows compatibility

--- winbuild.bat
+++ winbuild.bat
@@ -1,2 +1,2 @@
-dotnet.exe publish -r win-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true
-mv .\bin\Release\net5.0\win-x64\publish\coinmanager.exe .\exe\
+dotnet.exe publish -r win-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true /p:TargetPlatform=net5.0-windows
+move .\bin\Release\net5.0\win-x64\publish\coinmanager.exe .\exe\
